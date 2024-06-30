9a3b727c7c1990fa3ccba66a4fbfc4dd84d3c2dc
robinm2812 <robinm2812@gmail.com>
2024-06-29 22:31:38 -0400

    eslint prettier react

---
+++ react-intro/src/App.js
@@ -0,0 +1,30 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
+  ]);
+};
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "Arjun",
+      breed: "German Shepard",
+    }),
+    React.createElement(Pet, {
+      animal: "Parrot",
+      name: "Thathumae",
+      breed: "Rose Ringed Parakeets",
+    }),
+    React.createElement(Pet, {
+      animal: "Chicken",
+      name: "Korri",
+      breed: "Kerala Chicken",
+    }),
+  ]);
+};
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
