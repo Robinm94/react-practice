0c186f8c4717e0ab35f08746f281f49e43113517
robinm2812 <robinm2812@gmail.com>
2024-07-24 22:04:22 -0400

    Component composition

--- react-intro/src/SearchParams.jsx
+++ react-intro/src/SearchParams.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
 // useState hooks for saving data between react component render
 const ANIMALS = ["bird", "cat", "dog", "reptile"];
 const SearchParams = () => {
@@ -71,14 +71,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets}/>
     </div>
   );
 };
