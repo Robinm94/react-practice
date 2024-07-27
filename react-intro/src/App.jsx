e8a655f72f043d18793d8ad5e4c9487086d80e0b
robinm2812 <robinm2812@gmail.com>
2024-07-26 23:45:01 -0400

    React router

--- react-intro/src/App.jsx
+++ react-intro/src/App.jsx
@@ -1,15 +1,21 @@
-import ReactDOM from 'react-dom/client';
-import SearchParams from './SearchParams';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
 //sick
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams/>
-    </div>
-  )
-} 
+    <BrowserRouter>
+
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details/>}/>
+          <Route path="/" element={<SearchParams/>}/>
+        </Routes>
+    </BrowserRouter>
+  );
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App/>);
+root.render(<App />);
