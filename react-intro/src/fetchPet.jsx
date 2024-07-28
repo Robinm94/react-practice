279fb66a92d44888ed57480ec808cd5b276af431
robinm2812 <robinm2812@gmail.com>
2024-07-27 23:57:09 -0400

    React query and useParams

---
+++ react-intro/src/fetchPet.jsx
@@ -0,0 +1,11 @@
+const fetchPets = async ({ queryKey}) => {
+    const id = queryKey[1];
+    const apiRes = await fetch(`http://pets—v2.dev-apis.com/pets?id=${id}`);
+    if(!apiRes.ok){
+        throw new Error(`details/${id} fetch not ok`);
+    }
+
+    return apiRes.json();
+}
+
+export default fetchPets;
