7300b9ed6cd7f907cf3ac6c3c9bea3474ac392d6
robinm2812 <robinm2812@gmail.com>
2024-08-13 23:56:00 -0400

    Context

--- react-intro/src/App.jsx
+++ react-intro/src/App.jsx
@@ -1,6 +1,8 @@
-import ReactDOM from "react-dom/client";
+import { useState } from "react";
 import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
+import ReactDOM from "react-dom/client";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
@@ -14,16 +16,19 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const AdoptedPet = useState(null);
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
+        <AdoptedPetContext.Provider value={AdoptedPet}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
   );
