f31a2f32c6b809c55d88189f8e1a086c27d41d72
robinm2812 <robinm2812@gmail.com>
2024-08-14 00:13:22 -0400

    Use Context

--- react-intro/src/SearchParams.jsx
+++ react-intro/src/SearchParams.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
-import useBreedList from "./useBreedList";
-import Results from "./Results";
-import fetchSearch from "./fetchSearch";
+import { useState, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
+import Results from "./Results";
+import useBreedList from "./useBreedList";
+import fetchSearch from "./fetchSearch";
 // useState hooks for saving data between react component render
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
@@ -14,6 +15,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   // const [pets, setPets] = useState([]);
   const [breeds] = useBreedList(animal);
+  const [adoptedPet] = useContext(AdoptedPetContext);
 
   // useEffect(() => {
   //   requestPets();
@@ -45,6 +47,11 @@ const SearchParams = () => {
           setRequestParams(obj);
         }}
       >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
         <label htmlFor="location">
           Location
           <input name="location" id="location" placeholder="Location"></input>
